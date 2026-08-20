8f0903e921ba8f3a5b4af01ae61be11450eadc23
Artem-WebDeveloper <ave_art@icloud.com>
2026-08-20 21:23:36 +0300

    feat: implement globalErrorHandler and add AppError

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,11 +1,14 @@
 import 'reflect-metadata';
 import express from 'express';
 import userRouter from './modules/users/users.routes.ts';
+import globalErrorHandler from './middlewares/error.middleware.ts';
 
 const app = express();
 
 app.use(express.json({ limit: '10kb' }));
 
 app.use('/api/v1/users', userRouter);
+
+app.use(globalErrorHandler);
 
 export default app;
---
+++ backend/src/middlewares/error.middleware.ts
@@ -0,0 +1,50 @@
+import type { Request, Response, NextFunction } from 'express';
+
+interface CustomError extends Error {
+  statusCode?: number;
+  status?: string;
+  isOperational?: boolean;
+}
+
+function sendErrorDev(err: CustomError, res: Response) {
+  res.status(err.statusCode || 500).json({
+    status: err.status,
+    error: err,
+    message: err.message,
+    stack: err.stack,
+  });
+}
+
+function sendErrorProd(err: CustomError, res: Response) {
+  if (err.isOperational) {
+    res.status(err.statusCode || 500).json({
+      status: err.status,
+      message: err.message,
+    });
+  } else {
+    console.error('ERROR!', err);
+
+    res.status(500).json({
+      status: 'error',
+      message: 'Something went wrong!',
+    });
+  }
+}
+
+function globalErrorHandler(err: CustomError, req: Request, res: Response, next: NextFunction) {
+  err.statusCode = err.statusCode || 500;
+  err.status = err.status || 'error';
+
+  if (process.env.NODE_ENV === 'development') {
+    sendErrorDev(err, res);
+  } else if (process.env.NODE_ENV === 'production') {
+    const error: CustomError = { ...err };
+    error.message = err.message;
+    error.name = err.name;
+    error.isOperational = err.isOperational;
+
+    sendErrorProd(error, res);
+  }
+}
+
+export default globalErrorHandler;
