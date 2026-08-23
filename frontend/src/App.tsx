08a527d4a4a882218e7f927bdb5ef3ae88b6a861
Artem-WebDeveloper <ave_art@icloud.com>
2026-08-23 23:09:31 +0300

    feat: setup frontend infrastructure with shadcn/ui and dev proxy

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,5 +1,14 @@
+import { Button } from "./shared/ui/button";
+
 function App() {
-  return <p>Hello world!</p>;
+  return (
+    <div>
+      <p className="">Hello</p>
+      <Button variant="default" onClick={() => console.log("work")}>
+        BUTTON TEST
+      </Button>
+    </div>
+  );
 }
 
 export default App;
